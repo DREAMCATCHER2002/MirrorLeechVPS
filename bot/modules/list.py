723d6b3b3f76dd81a2b18da52db3fdf7433941c9
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-07-20 16:38:27 +0300

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -11,12 +11,12 @@ from bot.helper.telegram_helper import button_build
 def list_buttons(update, context):
     user_id = update.message.from_user.id
     if len(context.args) == 0:
-        return sendMessage('Send a search key along with command', context.bot, update.message)
+        return sendMessage('<b>📨 Send a search key along with command</b>', context.bot, update.message)
     buttons = button_build.ButtonMaker()
-    buttons.sbutton("Folders", f"types {user_id} folders")
-    buttons.sbutton("Files", f"types {user_id} files")
-    buttons.sbutton("Both", f"types {user_id} both")
-    buttons.sbutton("Cancel", f"types {user_id} cancel")
+    buttons.sbutton("📂 Folders 📂", f"types {user_id} folders")
+    buttons.sbutton("🗂️ Files 🗂️", f"types {user_id} files")
+    buttons.sbutton("📂 Both 🗂️", f"types {user_id} both")
+    buttons.sbutton("❌ Cancel ❌", f"types {user_id} cancel")
     button = InlineKeyboardMarkup(buttons.build_menu(2))
     sendMarkup('Choose option to list.', context.bot, update.message, button)
 
@@ -34,7 +34,7 @@ def select_type(update, context):
         return editMessage("List has been canceled!", msg)
     query.answer()
     item_type = data[2]
-    editMessage(f"<b>Searching for <i>{key}</i></b>", msg)
+    editMessage(f"<b>🔎 Searching for : {key}</b>", msg)
     Thread(target=_list_drive, args=(key, msg, item_type)).start()
 
 def _list_drive(key, bmsg, item_type):
@@ -44,7 +44,7 @@ def _list_drive(key, bmsg, item_type):
     if button:
         editMessage(msg, bmsg, button)
     else:
-        editMessage(f'No result found for <i>{key}</i>', bmsg)
+        editMessage(f'<b>No result found for : {key}</b>', bmsg)
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_buttons, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 list_type_handler = CallbackQueryHandler(select_type, pattern="types", run_async=True)
