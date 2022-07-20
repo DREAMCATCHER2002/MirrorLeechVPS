7b327f566ed9c8752cbb5021f48c16379fb821b4
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-07-20 16:50:45 +0300

    Update aria2_download.py

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -34,9 +34,9 @@ def __onDownloadStarted(api, gid):
                 if sname is not None:
                     smsg, button = GoogleDriveHelper().drive_list(sname, True)
                     if smsg:
-                        dl.getListener().onDownloadError('Someone already mirrored it for you !\n\n')
+                        dl.getListener().onDownloadError('<b>🧲 Someone already mirrored it for you..!! ')
                         api.remove([download], force=True, files=True)
-                        return sendMarkup("Here you go:", dl.getListener().bot, dl.getListener().message, button)
+                        return sendMarkup("<b>Here you go 👇</b>", dl.getListener().bot, dl.getListener().message, button)
             if any([ZIP_UNZIP_LIMIT, LEECH_LIMIT, TORRENT_DIRECT_LIMIT, STORAGE_THRESHOLD]):
                 sleep(1)
                 limit = None
